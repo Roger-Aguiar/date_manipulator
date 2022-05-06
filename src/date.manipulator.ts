42844d4598aea676e8c3073ac11558e221373221
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-05-06 17:33:22 -0300

    It creates the functions to sum days and months in the USA date format.

--- src/date.manipulator.ts
+++ src/date.manipulator.ts
@@ -1,4 +1,6 @@
 export class DateManipulator {
+    constructor(private fullDate?: string, private sumYear?: number, private sumMonth?: number, private sumDay?: number){}
+
   getNewUnitedStatesDate(): string {
     let date = new Date();
     let month: string =
@@ -14,4 +16,43 @@ export class DateManipulator {
 
     return fullDate;
   }
+
+  getNewBrazilianDate(): string {
+    let date = new Date();
+    let month: string =
+      date.getMonth() < 10
+        ? "0" + (date.getMonth() + 1).toString()
+        : date.getMonth().toString();
+    let day: string =
+      date.getDate() < 10
+        ? "0" + date.getDate().toString()
+        : date.getDate().toString();
+    let year: string = date.getFullYear().toString();
+    let fullDate = `${day}-${month}-${year}`;
+
+    return fullDate;
+  }
+
+  addYearsToUnitedStatesDate(): string{
+    let fullDateSplit = this.fullDate.split("-");    
+    return `${fullDateSplit[0]}-${fullDateSplit[1]}-${(+fullDateSplit[2] + this.sumYear).toString()}`      
+  }
+
+  addMonthsToUnitedStatesDate(): string{
+    let fullDateSplit = this.fullDate.split("-");    
+    return `${(+fullDateSplit[0] + this.sumMonth).toString()}-${fullDateSplit[1]}-${fullDateSplit[2].toString()}`      
+  }
+
+  addDaysToUnitedStatesDate(): string{
+    let fullDateSplit = this.fullDate.split("-");    
+    return `${fullDateSplit[0]}-${(+fullDateSplit[1] + this.sumDay).toString()}-${fullDateSplit[2].toString()}`      
+  }
+
+  addYearsToBrazilianDate(): string{
+    let fullDateSplit = this.fullDate.split("-");    
+    return `${fullDateSplit[0]}-${fullDateSplit[1]}-${(+fullDateSplit[2] + this.sumYear).toString()}`      
+  }
 }
+
+//This code gets the number of days in a specified month, the second parameter is the month, in this case, February
+//console.log(new Date(2022, 2, 0).getDate())
--- tests/date.manipulator.spec.ts
+++ tests/date.manipulator.spec.ts
@@ -1,14 +1,47 @@
 import { DateManipulator } from "../src/date.manipulator";
 
 describe("Date Manipulator Tests", () => {
-    let dateManipulator;
+  let dateManipulator;
+  let monthUSA = "10-06-2022";
+  let daysUSA = "05-11-2022";
+  let yearUSA = "05-06-2027";
+    
+  let yearBrazil = "06-05-2027";
+  
 
-    beforeEach(() => {
-        dateManipulator = new DateManipulator();
+  beforeEach(() => {
+    dateManipulator = new DateManipulator();
+  });
+
+  describe("Tests for United States date", () => {
+    it("Should return a new date in the Uniteds States format", () => {
+      expect(dateManipulator.getNewUnitedStatesDate()).not.toBeNull();
     });
 
-    it("Should return a new date", () => {
-        console.log(dateManipulator.getNewUnitedStatesDate());
-        expect(dateManipulator.getNewUnitedStatesDate()).not.toBeNull();
+    it("Should add a specified value into the year and return the new value when date is in the USA format", () => {
+      dateManipulator = new DateManipulator(dateManipulator.getNewUnitedStatesDate(), 5);
+      expect(dateManipulator.addYearsToUnitedStatesDate()).toBe(yearUSA);
     });
+
+    it("Should add a specified value into the day and return the new value when date is in the USA format", () => {
+        dateManipulator = new DateManipulator(dateManipulator.getNewUnitedStatesDate(), 0, 0, 5);
+        expect(dateManipulator.addDaysToUnitedStatesDate()).toBe(daysUSA);
+    });
+
+    it("Should add a specified value into the month and return the new value when date is in the USA format", () => {
+        dateManipulator = new DateManipulator(dateManipulator.getNewUnitedStatesDate(), 0, 5);
+        expect(dateManipulator.addMonthsToUnitedStatesDate()).toBe(monthUSA);
+    });
+  });
+
+  describe("Tests for Brazil date", () => {
+    it("Should return a new date in Brazilian format", () => {
+      expect(dateManipulator.getNewBrazilianDate()).not.toBeNull();
+    });
+
+    it("Should add a specified value into the year and return the new value when date is in BRAZILIAN format", () => {
+      dateManipulator = new DateManipulator(dateManipulator.getNewBrazilianDate(), 5);
+      expect(dateManipulator.addYearsToBrazilianDate()).toBe(yearBrazil);      
+    });
+  });
 });
